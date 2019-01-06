7fcab2594431fec1957a8bcf4de544e10bf2e49c
Michael Stewart <mstewart@mstewart-l.corp.instructure.com>
2019-01-05 18:44:56 -0700

    New files

---
+++ movies.js
@@ -0,0 +1,19 @@
+var movies = [
+    {
+	  title: "The Lord of the Rings: Fellowship of the Ring",
+	  rating: 5,
+	  have_seen: true
+	},
+
+    {
+	  title: "Freeway",
+	  rating: 3.5,
+	  have_seen: false
+	},
+
+    {
+	  title: "Casino Royal",
+	  rating: 4.5,
+	  have_seen: true
+	}
+]
