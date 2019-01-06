c963d4d7a9e120b7724ffee783017284b0cc9423
Michael Stewart <mstewart@mstewart-l.corp.instructure.com>
2019-01-05 19:25:58 -0700

    Updates

--- movies.js
+++ movies.js
@@ -15,5 +15,25 @@ var movies = [
 	  title: "Casino Royal",
 	  rating: 4.5,
 	  have_seen: true
-	}
+	},
+
+  {
+    title: "Django",
+    rating: 5,
+    have_seen: true
+  },
+
+  {
+    title: "The Good, The Bad, and The Ugly",
+    rating: 4.5,
+    have_seen: true
+  }
 ]
+
+for (var i = 0; i < movies.length; i++){
+  if(movies[i].have_seen === true){
+    console.log("You have seen " + "\"" + movies[i].title + "\"" + " - " + movies[i].rating + " Stars")
+  } else {
+    console.log("You have not seen " + "\"" + movies[i].title + "\"" + " - " + movies[i].rating + " Stars")
+  }
+}
